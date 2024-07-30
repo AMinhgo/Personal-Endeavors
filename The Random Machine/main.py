f62d54ce4dd9feb4b32db43a38709f2d2651a128
AMinhgo <137280280+AMinhgo@users.noreply.github.com>
2024-07-30 17:53:29 +0700

    Added option to generate duplicate

--- The Random Machine/main.py
+++ The Random Machine/main.py
@@ -5,6 +5,7 @@ def main():
     end_num = int(input("Enter End_num: "))
     num_of_results = int(input("Num of results: "))
     random_seed_ask = input("Random seed (yes, no)? ").lower()
+    allow_duplicates = input("Allow duplicates in input (yes, no)? ").lower()
     
     # set random seed so as to generate the same results if needed
     if random_seed_ask == "yes" or random_seed_ask == "y":
@@ -24,22 +25,36 @@ def main():
         result = random.randint(start_num, end_num)
         result_list.append(result)
         
-        # decide the order of the results, as well as remove any duplicates
-        if order == "asc" or order == "a":
-            result_list = list(set(result_list))
-        elif order == "desc" or order == "d":
-            result_list = list(set(result_list))
-            result_list.sort(reverse = True)
-        elif order == "rand" or order == "r":
-            result_list = list(dict.fromkeys(result_list))
+        # allow the user to choose whether to have duplicated results in their output
+        if allow_duplicates == "yes" or allow_duplicates == "y":
+            if order == "asc" or order == "a":
+                result_list = list(result_list)
+                result_list.sort(reverse = False)
+            elif order == "desc" or order == "d":
+                result_list = list(result_list)
+                result_list.sort(reverse = True)
+            elif order == "rand" or order == "r":
+                result_list = list(result_list)
+            else:
+                print("Error")
         else:
-            print("Error")
+        # decide the order of the results, as well as remove any duplicates
+            if order == "asc" or order == "a":
+                result_list = list(set(result_list))
+            elif order == "desc" or order == "d":
+                result_list = list(set(result_list))
+                result_list.sort(reverse = True)
+            elif order == "rand" or order == "r":
+                result_list = list(dict.fromkeys(result_list))
+            else:
+                print("Error")
         
         # make sure generated list is equal to number of results defined
         if len(result_list) == num_of_results:
             print(result_list)
             print("Random seed: " + str(random_seed))
             break 
+        
 
 if __name__ == "__main__":
     main()
